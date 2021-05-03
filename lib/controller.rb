d264faa52cdf279ea37cfbf931c65304f9bb59a6
HELENE-AUERBACH <ln_au@yahoo.fr>
2021-05-04 00:02:59 +0200

    Ajout de la fonctionnalité d'"Edition d'un potin en base" avec la journalisation de la base.

--- lib/controller.rb
+++ lib/controller.rb
@@ -5,20 +5,6 @@ class ApplicationController < Sinatra::Base
     erb :index
   end
 
-  get '/gossips/all/' do
-    erb :all_gossips, locals: {gossips: Gossip.all("gossip.csv")}
-  end
-
-  get '/gossips/:id/' do
-    if !params['id'].nil? && !params['id'].strip.empty? && params['id'] != "-1" && params['id'].to_i > 0
-      # on considère que l'utilisateur compte les potins à partir du numéro 1
-      puts "Voici le numéro du potin que tu veux : #{params['id']}!"
-      erb :show, locals: {gossip_hash: Gossip.find_gossip_with_index("gossip.csv", params['id'].to_i), id: params['id']}
-    else
-      puts "Tu n'as sélectionné aucun numéro de potin."
-    end
-  end
-
   get '/gossips/new/' do
     erb :new_gossip
   end
@@ -31,6 +17,36 @@ class ApplicationController < Sinatra::Base
     puts "De la bombe, et du coup ça, ça doit être ce que l'utilisateur a passé dans le champ gossip_content : #{params["gossip_content"]}"
     puts "Ça déchire sa mémé, bon allez je m'en vais du serveur, ciao les BGs !"
     Gossip.new(params["gossip_author"], params["gossip_content"]).save("gossip.csv")
+    redirect '/' # maintenant, redirige le user vers cette route
+  end
+
+  get '/gossips/all/' do
+    erb :all_gossips, locals: {gossips: Gossip.all("gossip.csv")}
+  end
+
+  get '/gossips/:id/' do
+    if !params['id'].nil? && !params['id'].strip.empty? && params['id'] != "-1" && params['id'].to_i > 0
+      # on considère que l'utilisateur compte les potins à partir du numéro 1
+      puts "Voici le numéro du potin que tu veux afficher : #{params['id']}!"
+      erb :show, locals: {gossip_hash: Gossip.find_gossip_with_index("gossip.csv", params['id'].to_i), id: params['id']}
+    else
+      puts "Tu n'as sélectionné aucun numéro de potin."
+    end
+  end
+
+  get '/gossips/:id/edit/' do
+    if !params['id'].nil? && !params['id'].strip.empty? && params['id'] != "-1" && params['id'].to_i > 0
+      puts "Voici le numéro du potin que tu veux éditer : #{params['id']}!"
+      erb :edit, locals: {gossip_hash: Gossip.find_gossip_with_index("gossip.csv", params['id'].to_i), id: params['id']}
+    else
+      puts "Tu n'as sélectionné aucun numéro de potin."
+    end
+  end
+
+  post '/gossips/:id/edit/' do
+    puts "Ce programme sauvegarde en base un potin mis à jour."
+    puts "Ceci est le contenu du hash params : #{params}"
+    Gossip.update("gossip.csv", params["index"], params["gossip_author"], params["gossip_content"])
     redirect '/' # maintenant, redirige le user vers cette route
   end
 end
--- lib/gossip.rb
+++ lib/gossip.rb
@@ -21,7 +21,7 @@ class Gossip
         # A comma and quote needs to be encapsulated and quoted, so "," becomes """,""".
       end
     else
-      puts "Impossible de créer un fichier de nom \"./db/#{PREFIX}#{file_name}\"."
+      puts "Impossible de créer/ajouter dans un fichier de nom \"./db/#{PREFIX}#{file_name}\"."
     end
   end
   
@@ -59,7 +59,50 @@ class Gossip
     return result
   end
 
+  def self.update(file_name, index, new_author, new_content)
+    new_gossips_array = [] # création d'un Array d'instances de la classe Gossip, vide, qui s'appelle new_gossips_array
+    puts "index : #{index}, new_author : #{new_author}, content : #{new_content}"
+    if Gossip.check_file_name?(file_name) # On vérifie le nom du fichier à consulter via une méthode check_file_name? (cf. plus bas)
+      CSV.foreach("./db/#{PREFIX}#{file_name}").with_index do |row, row_number| # le chemin relatif est donné par rapport au répertoire d'où est lancée l'application controller.rb
+        print "row_number : #{row_number}"
+        if row_number == index.to_i
+          puts " => (#{new_author}, #{new_content})"
+          new_gossips_array << Gossip.new(new_author, new_content)
+          # rajoute au Array "new_gossips_array" un nouveau gossip de même index, mais avec un nouvel author et un nouveau content
+        else
+          puts " => (#{row[0]}, #{row[1]})"
+          new_gossips_array << Gossip.new(row[0], row[1])
+          # rajoute au Array "new_gossips_array" un nouveau gossip de même index, même author et même content
+        end
+      end
+      Gossip.save_new_file(file_name, new_gossips_array)
+    else
+      puts "Impossible de consulter un fichier de nom \"./db/#{PREFIX}#{file_name}\"."
+    end
+  end
+
   private
+  
+  def self.save_new_file(file_name, new_gossips_array)
+    if Gossip.check_file_name?(file_name) # On vérifie le nom du fichier à créer via une méthode check_file_name? (cf. plus bas)
+      previous_version_number = 0
+      while File.file?("./db/#{PREFIX}#{previous_version_number.to_s}_#{file_name}")
+        previous_version_number += 1
+      end
+      File.rename("./db/#{PREFIX}#{file_name}", "./db/#{PREFIX}#{previous_version_number.to_s}_#{file_name}")
+      CSV.open("./db/#{PREFIX}#{file_name}", "wb") do |csv_file| # le chemin relatif est donné par rapport au répertoire d'où est lancée l'application controller.rb
+        new_gossips_array.length.times do |gossip_index|
+          puts "gossip_index : #{gossip_index} => [#{new_gossips_array[gossip_index].author}, #{new_gossips_array[gossip_index].content}]"
+          csv_file << [new_gossips_array[gossip_index].author, new_gossips_array[gossip_index].content]
+          # To encode a quote, use ". One double quote symbol in a field will be encoded as "", and the whole field will become """".
+          # A comma is simply encapsulated using quotes, so , becomes ",".
+          # A comma and quote needs to be encapsulated and quoted, so "," becomes """,""".
+        end
+      end
+    else
+      puts "Impossible de créer un fichier de nom \"./db/#{PREFIX}#{file_name}\"."
+    end
+  end
 
   def self.check_file_name?(file_name)
     # Vérification simple du format du nom du fichier.
